8405880dfcab3abc9d2f9d3b821ec5104326563e
azharrivaldi <azharrivaldi1234@gmail.com>
2024-12-08 17:09:37 +0700

    image downloade

--- lib/service/network_service.dart
+++ lib/service/network_service.dart
@@ -4,7 +4,7 @@ import '../model/model_home.dart';
 
 class NetworkService {
   static Future<List<ModelHome>> fetchNews(String kategori) async {
-    String baseUrl = 'https://pixabay.com/api/?key=12986876-82bf5b8a7f2948cd66ffd41f1&image_type=all&lang=id&category=$kategori';
+    String baseUrl = 'https://pixabay.com/api/?key=API KEY&image_type=all&lang=id&category=$kategori';
     try {
       final response = await http.get(Uri.parse(baseUrl));
       List listData = (jsonDecode(response.body) as Map<String, dynamic>)['hits'];
